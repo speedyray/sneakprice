9ec0eb203b3aee2c035410d53594f75bf4d86e8b
Raynold <speedyray2ray@gmail.com>
2026-02-21 19:07:21 -0500

    Added scan folder and files

---
+++ app/scan/page.tsx
@@ -0,0 +1,9 @@
+import SneakerScanner from "@/components/SneakerScanner";
+
+export default function ScanPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <SneakerScanner />
+    </div>
+  );
+}
