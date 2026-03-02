002a2299e72745968e195df2b52497ec327e3600
Raynold <speedyray2ray@gmail.com>
2026-03-01 20:57:22 -0500

    Added admin authentication

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    const authHeader = request.headers.get("authorization");
+
+    if (authHeader) {
+      const token = authHeader.split(" ")[1];
+      const decoded = Buffer.from(token, "base64").toString("utf-8");
+      const [user, pass] = decoded.split(":");
+
+      if (
+        user === process.env.ADMIN_USER &&
+        pass === process.env.ADMIN_PASSWORD
+      ) {
+        return NextResponse.next();
+      }
+    }
+
+    return new NextResponse("Authentication required", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
+  return NextResponse.next();
+}
