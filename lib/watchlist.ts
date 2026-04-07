421c6df93c85e57824ffb86c03d97570b223fee7
Raynold <speedyray2ray@gmail.com>
2026-04-06 20:01:39 -0400

    feat: add sneaker watchlist for deal pipeline

---
+++ lib/watchlist.ts
@@ -0,0 +1,75 @@
+// lib/watchlist.ts
+// Top sneaker models scanned every 20 minutes by the deal pipeline.
+// Add/remove models here to control the background scan scope.
+
+export const WATCHLIST: string[] = [
+  // Nike Dunks
+  "Nike Dunk Low Panda",
+  "Nike Dunk Low Grey Fog",
+  "Nike Dunk Low Photon Dust",
+  "Nike Dunk Low Retro White Black",
+  "Nike Dunk High Pro",
+  "Nike Dunk Low Championship Red",
+  "Nike Dunk Low UNC",
+
+  // Air Jordan 1
+  "Air Jordan 1 Retro High OG Chicago",
+  "Air Jordan 1 Retro High OG University Blue",
+  "Air Jordan 1 Retro High OG Bred Toe",
+  "Air Jordan 1 Low",
+  "Air Jordan 1 Mid",
+
+  // Air Jordan 3
+  "Air Jordan 3 White Cement",
+  "Air Jordan 3 Black Cement",
+  "Air Jordan 3 Fire Red",
+  "Air Jordan 3 True Blue",
+
+  // Air Jordan 4
+  "Air Jordan 4 Retro Bred",
+  "Air Jordan 4 Retro Military Black",
+  "Air Jordan 4 Retro Red Thunder",
+  "Air Jordan 4 Retro White Cement",
+  "Air Jordan 4 Retro Fire Red",
+
+  // Yeezy
+  "Adidas Yeezy Boost 350 V2 Zebra",
+  "Adidas Yeezy Boost 350 V2 Bred",
+  "Adidas Yeezy Boost 350 V2 Static",
+  "Adidas Yeezy Boost 350 V2 Beluga",
+  "Adidas Yeezy 700 Wave Runner",
+  "Adidas Yeezy 700 Vanta",
+  "Adidas Yeezy Slide Pure",
+
+  // Adidas
+  "Adidas Samba OG White",
+  "Adidas Samba OG Black",
+  "Adidas Gazelle Indoor",
+  "Adidas Campus 00s",
+  "Adidas Stan Smith",
+  "Adidas Superstar",
+
+  // New Balance
+  "New Balance 550 White Green",
+  "New Balance 550 White Black",
+  "New Balance 990v5",
+  "New Balance 2002R",
+  "New Balance 574",
+  "New Balance 1906R",
+
+  // Nike Air Force 1
+  "Nike Air Force 1 Low White",
+  "Nike Air Force 1 Low Black",
+
+  // Nike Air Max
+  "Nike Air Max 90",
+  "Nike Air Max 1",
+  "Nike Air Max 97",
+  "Nike Air Max Plus",
+
+  // Jordan Brand Other
+  "Air Jordan 5 Retro Metallic",
+  "Air Jordan 6 Retro Carmine",
+  "Air Jordan 11 Retro Concord",
+  "Air Jordan 11 Retro Bred",
+];
